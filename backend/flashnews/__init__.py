6821bf0311af954f107d76eefdbfebcdd17823d4
Furuta-Mototada <mototada@uni.minerva.edu>
2024-11-05 14:32:04 -0300

    added blueprint, login manager, database initialization

--- backend/flashnews/__init__.py
+++ backend/flashnews/__init__.py
@@ -13,6 +13,9 @@ def create_app():
     app.config["SECRET_KEY"] = "dev"
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
     app.config["debug"] = True
+    app.config["SECRET_KEY"] = "dev"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+    app.config["debug"] = True
 
     db.init_app(app)
 
@@ -26,27 +29,27 @@ def create_app():
         return User.query.get(int(user_id))
 
     # blueprint for auth routes in our app
-    # from .auth import auth as auth_blueprint
-    # app.register_blueprint(auth_blueprint)
+    from .auth import auth as auth_blueprint
+
+    app.register_blueprint(auth_blueprint)
 
     # blueprint for user routes in our app
-    # from .user import users as user_blueprint
-    # app.register_blueprint(user_blueprint)
+    from .user import users as user_blueprint
+
+    app.register_blueprint(user_blueprint)
 
     # blueprint for post routes in our app
-    # from .post import posts as posts_blueprint
-    # app.register_blueprint(posts_blueprint)
+    from .post import posts as posts_blueprint
+
+    app.register_blueprint(posts_blueprint)
 
     # blueprint for collection routes in our app
-    # from .collections import collections as collections_blueprint
-    # app.register_blueprint(collections_blueprint)
+    from .collections import collections as collections_blueprint
+
+    app.register_blueprint(collections_blueprint)
 
     # Initialize the database
     with app.app_context():
         db.create_all()
-
-    @app.route("/")
-    def home():
-        return "Hello, Flask!"
 
     return app
