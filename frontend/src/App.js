ca44623bb88cd6cef647e9ddf27f7c97d3bad900
Tea Pei Qi <47902092+kyurikotpq@users.noreply.github.com>
2024-11-11 08:15:28 -0800

    chore: Themed Header with working links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import AuthPage from './pages/AuthPage';
-import FeedPage from './pages/FeedPage';
-import CollectionsPage from './pages/CollectionsPage';
-import ProfilePage from './pages/ProfilePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import "./App.css";
+import Header from "./components/Header";
+import AuthPage from "./pages/AuthPage";
+import FeedPage from "./pages/FeedPage";
+import CollectionsPage from "./pages/CollectionsPage";
+import ProfilePage from "./pages/ProfilePage";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#456268",
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/login" element={<AuthPage />} />
-          <Route path="/feed" element={<FeedPage />} />
-          <Route path="/collections" element={<CollectionsPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/login" element={<AuthPage />} />
+            <Route path="/feed" element={<FeedPage />} />
+            <Route path="/collections" element={<CollectionsPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
