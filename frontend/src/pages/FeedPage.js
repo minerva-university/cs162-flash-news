1c877eca646b2b4a8907ea3e0af682f1da71e63c
Tea Pei Qi <47902092+kyurikotpq@users.noreply.github.com>
2024-11-11 18:22:17 -0800

    interim: Incomplete AddPostForm func

--- frontend/src/pages/FeedPage.js
+++ frontend/src/pages/FeedPage.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { styled } from "@mui/material/styles";
-import Masonry from "@mui/lab/Masonry";
 import Box from "@mui/material/Box";
 import PostController from "../controllers/PostController";
 import PostCard from "../components/PostCard";
+import AddPostForm from "../forms/AddPostForm";
 
 const FeedPage = () => {
   const [posts, setPosts] = useState([]);
@@ -15,14 +14,11 @@ const FeedPage = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Feed</h1>
-      <p>Render the form to add a post</p>
-      <Box style={{ padding: 20 }}>
-        {posts &&
-          posts.map((post) => <PostCard key={post.post_id} post={post} />)}
-      </Box>
-    </div>
+    <Box style={{ padding: 20 }}>
+      <AddPostForm />
+      {posts &&
+        posts.map((post) => <PostCard key={post.post_id} post={post} />)}
+    </Box>
   );
 };
 
