75536c98aacb60e2e136d95a7a8ffc7b6f08ffcc
Tea Pei Qi <47902092+kyurikotpq@users.noreply.github.com>
2024-12-02 12:01:19 -0800

    feat: Call to Flask backend for OpenGraph parsing

--- frontend/src/controllers/PostController.js
+++ frontend/src/controllers/PostController.js
@@ -72,6 +72,25 @@ class PostController {
     //   throw new Error(`${responseBody.message}`);
     // }
   }
+
+  static async getOGMetadata(url) {
+    // @TODO: Standardize the DB_HOST value e.g. in config file
+    // Also, for some reason localhost:5000 is not working
+    const DB_HOST = "http://127.0.0.1:5000/api";
+    const response = await fetch(`${DB_HOST}/og/url`, {
+      method: "POST",
+      body: JSON.stringify({ url }),
+      headers: { "content-type": "application/json" },
+      // headers: HEADERS_WITH_JWT(user),
+    });
+
+    const responseBody = await response.json();
+    if (response?.ok) {
+      return responseBody;
+    } else {
+      throw new Error(`${responseBody.message}`);
+    }
+  }
 }
 
 export default PostController;
--- frontend/src/forms/AddPostForm.js
+++ frontend/src/forms/AddPostForm.js
@@ -4,21 +4,33 @@ import CardHeader from "@mui/material/CardHeader";
 import { TextareaAutosize } from "@mui/base/TextareaAutosize";
 
 import Avatar from "@mui/material/Avatar";
-import { Box, Button, Modal } from "@mui/material";
+import {
+  Box,
+  Button,
+  InputLabel,
+  FormControl,
+  Modal,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import TagsController from "../controllers/TagsController";
 import MultipleSelectChip from "../components/MultipleSelectChip";
+import PostController from "../controllers/PostController";
 
+// Style for the modal
 const style = {
   position: "absolute",
-  top: "50%",
   left: "50%",
-  transform: "translate(-50%, -50%)",
+  transform: "translate(-50%, 0)",
   width: "50%",
   maxWidth: "555px",
   bgcolor: "background.paper",
   boxShadow: 24,
   outline: "none",
   p: 4,
+  // padding: "1rem",
+  margin: "1rem",
 };
 
 export default function AddPostForm() {
@@ -27,6 +39,11 @@ export default function AddPostForm() {
   const [open, setOpen] = useState(false);
   const [categories, setCategories] = useState([]);
   const [selectedCategories, setSelectedCategories] = useState([]);
+  const [postVisibility, setPostVisibility] = useState("public");
+  const [link, setLink] = useState("");
+  const [linkError, setLinkError] = useState(false);
+
+  const [ogMetadata, setOgMetadata] = useState(null);
 
   const handleOpen = async () => {
     // Reset inputs
@@ -42,23 +59,74 @@ export default function AddPostForm() {
   };
 
   const handleClose = () => {
-    // @TODO: Nicer confirmation modal
-    // const confirmation = confirm("Are you sure you want to exit? All changes will be lost.");
-    // if (confirmation)
+    const confirmation = window.confirm(
+      "Are you sure you want to exit? All changes will be lost."
+    );
+    if (confirmation) {
+      // Reset data
+      setOgMetadata(null);
+      setLink("");
+      setLinkError(false);
+      setCategories([]);
+      mainTextareaRef.current.value = "";
 
+      // Close the modal
+      setOpen(false);
+    }
+  };
+
+  const handleSave = () => {
+    if (!mainTextareaRef.current.value) {
+      alert("Please type a few meaningful words!");
+      return;
+    }
+
+    if (!link) {
+      alert("Please provide a link!");
+      return;
+    }
+
+    // Save the post
+    const post = {
+      content: mainTextareaRef.current.value,
+      categories: selectedCategories,
+      visibility: postVisibility,
+    };
+    console.log(post);
     setOpen(false);
   };
 
-  // const handleTagAdd = (event) => {
-  //   if (event.key === "Enter" && tagInput.trim() !== "") {
-  //     setTagsData([...tags, { key: tags.length, label: tagInput }]);
-  //     setTagInput("");
-  //   }
-  // };
+  const handleVisibilityChange = (event) => {
+    console.log(event.target.value);
+    setPostVisibility(event.target.value);
+  };
 
-  // const handleTagDelete = (tagToDelete) => () => {
-  //   setTagsData((tags) => tags.filter((tag) => tag.key !== tagToDelete.key));
-  // };
+  function isValid(url) {
+    const pattern = /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-]*)*$/;
+    return pattern.test(url);
+  }
+
+  const parseLink = (event) => {
+    const url = event.target.value;
+    if (!url || !isValid(url)) {
+      setLinkError(true);
+      setOgMetadata(null);
+      return;
+    }
+
+    setLinkError(false);
+    setLink(url);
+    getOGMetadata(url);
+  };
+
+  const getOGMetadata = async (url) => {
+    PostController.getOGMetadata(url)
+      .then((metadata) => {
+        console.log(metadata);
+        setOgMetadata(metadata);
+      })
+      .catch((error) => console.error(error));
+  };
 
   return (
     <Card sx={{ width: "50%", maxWidth: "555px", margin: "0 auto 3rem" }}>
@@ -76,48 +144,116 @@ export default function AddPostForm() {
         sx={{ cursor: "pointer", userSelect: "none" }}
         onClick={handleOpen}
       />
+
+      {/* New Post Modal */}
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
+        sx={{ overflowY: "scroll" }}
       >
         <Box sx={style}>
-          <Box sx={{ display: "flex", border: "1px" }}>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              border: "1px",
+              marginBottom: "1rem",
+            }}
+          >
             <Avatar
               src={profile_picture ?? ""}
-              sx={(theme) => ({ bgcolor: theme.palette.primary.main })}
+              sx={(theme) => ({
+                bgcolor: theme.palette.primary.main,
+                marginRight: "1rem",
+              })}
               aria-label="User Profile"
             >
               R
             </Avatar>
-            
+            <TextField
+              fullWidth
+              id="link"
+              label="Share a Link"
+              variant="outlined"
+              onChange={parseLink}
+              onKeyDown={(e) =>
+                e.key === "Enter" && getOGMetadata(e.target.value)
+              }
+              error={linkError}
+              helperText={linkError && "Please enter a valid link"}
+              required
+            />
           </Box>
-          {/* Main Textarea */}
-          <TextareaAutosize
-            style={{
-              width: "100%",
-              border: "none",
-              outline: "none",
-              marginTop: "1rem",
-              marginBottom: "1rem",
-            }}
-            ref={mainTextareaRef}
-            aria-label="minimum height"
-            minRows={7}
-            placeholder="What's on your mind?"
-          />
-          {/* Categories */}
-          <MultipleSelectChip
-            id="categories-list"
-            label="Select Categor(ies)"
-            options={categories}
-            max={5}
-            sx={{ marginBottom: "1rem" }}
-            onChange={(selected) => setSelectedCategories(selected)}
-          />
-          {/* Save Post */}
-          <Button sx={{ marginLeft: "auto" }}>Save</Button>
+          {link != "" && (
+            <>
+              {/* OG Image (if any) */}
+              {ogMetadata?.image && (
+                <img
+                  src={ogMetadata.image}
+                  alt={ogMetadata.title}
+                  style={{ height: "300px", width: "100%", objectFit: "cover" }}
+                />
+              )}
+
+              <h2>{ogMetadata?.title}</h2>
+
+              {/* Main Textarea */}
+              <TextareaAutosize
+                style={{
+                  width: "100%",
+                  border: "none",
+                  outline: "none",
+                  marginTop: "1rem",
+                  marginBottom: "1rem",
+                }}
+                ref={mainTextareaRef}
+                aria-label="minimum height"
+                minRows={7}
+                placeholder="What's on your mind?"
+              />
+              {/* Categories */}
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  marginBottom: "1rem",
+                }}
+              >
+                <MultipleSelectChip
+                  id="categories-list"
+                  label="Select Categories"
+                  options={categories}
+                  max={5}
+                  sx={{ marginRight: "1rem" }}
+                  onChange={(selected) => setSelectedCategories(selected)}
+                />
+                <FormControl sx={{ width: "50%" }}>
+                  <InputLabel id="post-visibility-label">
+                    Post Visibility
+                  </InputLabel>
+                  <Select
+                    labelId="post-visibility-label"
+                    id="post-visibility"
+                    value={postVisibility}
+                    label="Post Visibility"
+                    onChange={handleVisibilityChange}
+                  >
+                    <MenuItem value={"public"}>Public</MenuItem>
+                    <MenuItem value={"private"}>Private</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+              {/* Save Post */}
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button sx={{ marginLeft: "auto" }} onClick={handleSave}>
+                  Save
+                </Button>
+              </Box>
+            </>
+          )}
         </Box>
       </Modal>
     </Card>
