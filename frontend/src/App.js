2d820195a652c8613a33dab547b081f2fc82cc3a
laryssacoe <laryssa@uni.minerva.edu>
2024-12-10 01:06:46 -0300

    fix: integrate with user -- remove hard-coded auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,23 +26,18 @@ function App() {
   const location = useLocation();
   const publicRoutes = ["/login", "/signup"];
 
-  const currentUser = {
-    user_id: 1, // Hardcoded user ID
-    username: "testuser", // Hardcoded username
-  };
-
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-        {!publicRoutes.includes(location.pathname) && <Header currentUser={currentUser} />}
+        {!publicRoutes.includes(location.pathname) && <Header />}
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/feed" element={<FeedPage />} />
           <Route path="/post/:id" element={<PostDetailPage />} />
-          <Route path="/user/:username/collections" element={<CollectionsPage currentUser={currentUser} />} />
-          <Route path="/collections/:id/:title" element={<CollectionDetailModal currentUser={currentUser} />} />
+          <Route path="/user/:username/collections" element={<CollectionsPage />} />
+          <Route path="/collections/:id/:title" element={<CollectionDetailModal />} />
           <Route path="/:username/settings" element={<SettingsPage />} />
-          <Route path="/:username/profile" element={<ProfilePage currentUser={currentUser} />} />
+          <Route path="/:username/profile" element={<ProfilePage />} />
           <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </ThemeProvider>
